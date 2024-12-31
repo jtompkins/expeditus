bb555f37b9916e7b57493e81d2e0c4292ebcd279
Josh Tompkins <me@joshtompkins.codes>
2024-12-31 15:13:57 -0600

    Login doesn't need JSX

--- src/routes/core.ts
+++ src/routes/core.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono"
 import { getConnInfo } from "hono/deno"
 import { MetricRepository } from "../repos/metricrepository.ts"
 import { UrlRepository } from "../repos/urlrepository.ts"
-import { Login } from "../views/login.tsx"
+import { Login } from "../views/login.ts"
 import { AppVariables } from "./env.ts"
 
 const core = new Hono<{ Variables: AppVariables }>()
@@ -15,7 +15,7 @@ core.get("/", (c) => {
     return c.redirect("/app")
   }
 
-  return c.html(<Login />)
+  return c.html(Login())
 })
 
 core.get("/:slug", (c) => {
@@ -27,6 +27,8 @@ core.get("/:slug", (c) => {
   const url = urlRepo.getBySlug(slug)
 
   if (url) {
+    urlRepo.incrementViewsById(url.id)
+
     const info = getConnInfo(c)
 
     metricRepo.createMetric(url.id, info.remote.address, c.req.raw.referrer)
